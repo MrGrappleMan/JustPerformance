7e51b0032cf7806436823298243cb37b010ae82f
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-03-29 18:21:13 +0530

    Update LX.sh

--- LX.sh
+++ LX.sh
@@ -8,7 +8,7 @@ sudo add-apt-repository multiverse -y
 sudo add-apt-repository ppa:alessandro-strada/google-drive-ocamlfuse-beta -y
 sudo add-apt-repository ppa:costamagnagianfranco/boinc -y
 sudo add-apt-repository ppa:gezakovacs/ppa -y
-sudo apt-fast install preload redshift-gtk snowflake-proxy tor obfs4proxy util-linux zram-config package-update-indicator system-monitoring-center synaptic unetbootin -y
+sudo apt-fast install preload redshift-gtk snowflake-proxy tor obfs4proxy util-linux zram-config package-update-indicator system-monitoring-center unetbootin nvidia-cuda-toolkit ocl-icd-libopencl1 opencl-icd -y
 sudo apt-fast purge thunderbird compiz-core -y
 sudo systemctl enable --now preload
 sudo swapoff /swapfile
