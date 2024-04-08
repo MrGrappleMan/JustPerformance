fde78c2e6123a804926527b761a9946315561b2a
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-04-08 12:19:04 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo apt-get 
+sudo apt-get update
+sudo apt-get install curl -y
 sudo bash -c "$(curl -sL https://git.io/vokNn)"
 sudo apt-fast install software-properties-common -y
 sudo add-apt-repository main -y
