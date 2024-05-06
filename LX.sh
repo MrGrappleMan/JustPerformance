5eeb17b5aaa9a2b6869b188b3da599d70c806ffb
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-05-07 02:42:56 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -1,4 +1,4 @@
-cd 
+cd /tmp/
 clear
 apt update
 apt install sudo -y
@@ -14,8 +14,7 @@ sudo add-apt-repository universe -y >/dev/null 2>&1
 sudo add-apt-repository multiverse -y >/dev/null 2>&1
 sudo curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg >/dev/null 2>&1
 sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/ >/dev/null 2>&1
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list' >/dev/null 2>&1
-sudo rm microsoft.gpg >/dev/null 2>&1
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
 sudo apt-fast update
 sudo apt-fast install microsoft-edge-dev coreutils snowflake-proxy tor obfs4proxy util-linux zram-config -y >/dev/null 2>&1
 sudo apt-fast purge firefox -y >/dev/null 2>&1
