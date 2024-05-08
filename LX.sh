59f3c06517ea04e2f95103a1544c71370bd2bbe0
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-05-09 00:34:35 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -12,7 +12,6 @@ sudo add-apt-repository main -y
 sudo add-apt-repository restricted -y
 sudo add-apt-repository universe -y
 sudo add-apt-repository multiverse -y
-sudo add-apt-repository partner -y
 sudo apt-fast update
 sudo apt-fast install coreutils util-linux synaptic zram-config -y
 sudo apt-fast install snowflake-proxy tor obfs4proxy -y >/dev/null 2>&1
