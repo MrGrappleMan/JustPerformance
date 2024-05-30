027e761b3c9931d1ed007e8cf6a345c1869b752e
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-05-30 20:52:03 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -14,7 +14,6 @@ sudo add-apt-repository restricted -y >/dev/null 2>&1
 sudo add-apt-repository universe -y >/dev/null 2>&1
 sudo add-apt-repository multiverse -y >/dev/null 2>&1
 sudo apt-fast update >/dev/null 2>&1
-sudo apt-fast install  -y >/dev/null 2>&1
 for ipkg in coreutils util-linux zram-config snowflake-proxy tor obfs4proxy; do sudo apt-fast install $ipkg -y; done
 for rpkg in package-update-indicator; do sudo apt-fast remove $rpkg -y; done
 # ----------------------------------------------------------------------------------------------------
