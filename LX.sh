77dc0a1c87936d9743fd78be8f61c7edd571b00b
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-05-31 16:42:05 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -152,6 +152,8 @@ cat <<EOF | sudo crontab -
 @hourly (sudo apt-fast update >/dev/null 2>&1; sudo apt-fast dist-upgrade -y >/dev/null 2>&1; sudo apt-fast autoclean >/dev/null 2>&1)
 EOF
 # ----------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------
 sudo apt-fast update >/dev/null 2>&1
 sudo apt-fast dist-upgrade -y >/dev/null 2>&1
 cd
