effdb41d70195929e04d4d52d8d47f340e55b9ab
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-05-31 20:18:51 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -161,7 +161,9 @@ nameserver 8.8.4.4
 nameserver 1.1.1.1
 nameserver 1.0.0.1
 nameserver 9.9.9.9
+nameserver 149.112.112.112
 nameserver 208.67.222.222
+nam
 nameserver 192.71.245.208
 nameserver 91.239.100.100
 nameserver 45.33.97.5
