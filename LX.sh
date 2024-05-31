8d32b598149b15841d1bfb7d9b4371f9dbdbbeca
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-05-31 19:51:50 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -161,8 +161,8 @@ nameserver 8.8.8.8
 nameserver 9.9.9.9
 nameserver 208.67.222.222
 nameserver 192.71.245.208
-nameserver 1.1.1.1
-nameserver 1.1.1.1
+nameserver 91.239.100.100
+nameserver 91.
 EOL
 # ----------------------------------------------------------------------------------------------------
 sudo apt-fast update >/dev/null 2>&1
