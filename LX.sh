c7a574b67ae92c5639cb8b28d9039558a46f10d5
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-06-03 14:58:54 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -15,7 +15,7 @@ sudo add-apt-repository universe -y >/dev/null 2>&1
 sudo add-apt-repository multiverse -y >/dev/null 2>&1
 sudo apt-fast update >/dev/null 2>&1
 for ipkg in kde-full coreutils resolvconf nvidia-cuda-toolkit ocl-icd-libopencl1 opencl-icd util-linux plymouth zram-config snowflake-proxy tor obfs4proxy; do sudo apt-fast install $ipkg -y >/dev/null 2>&1; done
-for rpkg in package-update-indicator; do sudo apt-fast remove $rpkg -y >/dev/null 2>&1; done
+for rpkg in xfconf xfce4-utils xfwm4 xfce4-session xfdesktop4 exo-utils xfce4-panel x package-update-indicator; do sudo apt-fast remove $rpkg -y >/dev/null 2>&1; done
 # ----------------------------------------------------------------------------------------------------
 sudo systemctl enable --now zram-config >/dev/null 2>&1
 sudo systemctl stop zram-config >/dev/null 2>&1
