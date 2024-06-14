3492a665da7a177cf7ed12a34ec235a1614ee6cf
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-06-14 13:29:26 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -19,7 +19,7 @@ sudo add-apt-repository ppa:system76-dev/pre-stable -y >/dev/null 2>&1
 sudo sh -c 'echo "deb http://archive.neon.kde.org/user focal main" > /etc/apt/sources.list.d/neon.list'
 sudo wget -qO - http://archive.neon.kde.org/public.key | sudo apt-key add - >/dev/null 2>&1
 sudo apt-fast update >/dev/null 2>&1
-for ipkg in systemd system76-driver system76-driver-nvidia ubuntu-drivers-common lcoreutils flatpak resolvconf nvidia-cuda-toolkit ocl-icd-libopencl1 opencl-icd util-linux plymouth zram-config snowflake-proxy tor obfs4proxy; do sudo apt-fast install $ipkg -y; done
+for ipkg in systemd system76-driver system76-driver-nvidia ubuntu-drivers-common coreutils flatpak resolvconf nvidia-cuda-toolkit ocl-icd-libopencl1 opencl-icd util-linux plymouth zram-config snowflake-proxy tor obfs4proxy; do sudo apt-fast install $ipkg -y; done
 for rpkg in package-update-indicator; do sudo apt-fast remove $rpkg -y --autoremove; done
 # Flatpak----------------------------------------------------------------------------------------------------
 sudo flatpak remote-add --if-not-exists --noninteractive flathub https://dl.flathub.org/repo/flathub.flatpakrepo >/dev/null 2>&1
