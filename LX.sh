ddc66a4bf015b26b74b3b7032b99391df85bffa7
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-06-15 01:25:06 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -20,9 +20,9 @@ sudo sh -c 'echo "deb http://archive.neon.kde.org/user focal main" > /etc/apt/so
 sudo wget -qO - http://archive.neon.kde.org/public.key | sudo apt-key add - >/dev/null 2>&1
 sudo apt-fast update >/dev/null 2>&1
 # Drivers
-ubuntu-drivers-common system76-driver system76-driver-nvidia nvidia-cuda-toolkit ocl-icd-libopencl1 opencl-icd
+for syspkg in ubuntu-drivers-common system76-driver system76-driver-nvidia nvidia-cuda-toolkit ocl-icd-libopencl1 opencl-icd
 # System
-for ipkg in systemd coreutils flatpak resolvconf util-linux zram-config snowflake-proxy tor obfs4proxy; do sudo apt-fast install $ipkg -y; done
+for syspkg in systemd coreutils flatpak resolvconf util-linux zram-config snowflake-proxy tor obfs4proxy; do sudo apt-fast install $syspkg -y; done
 # Flatpak----------------------------------------------------------------------------------------------------
 sudo flatpak remote-add --if-not-exists --noninteractive flathub https://dl.flathub.org/repo/flathub.flatpakrepo >/dev/null 2>&1
 sudo flatpak remote-add --if-not-exists --noninteractive flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo >/dev/null 2>&1
