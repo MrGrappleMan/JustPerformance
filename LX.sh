045ec21658061390bc5a53972ad946d24547f034
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-06-16 23:42:28 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -1,43 +1,43 @@
 clear
 echo Procedure will take a long time to complete.
 echo Please wait...
-apt-get update >/dev/null 2>&1
-apt-get install sudo -y >/dev/null 2>&1
-sudo apt-get update >/dev/null 2>&1
-for epkg in curl git dkms wget build-essential; do sudo apt install $epkg -y >/dev/null 2>&1; done
-sudo /bin/bash -c "$(sudo curl -sL https://raw.githubusercontent.com/ilikenwf/apt-fast/master/quick-install.sh)" >/dev/null 2>&1
+apt-get update
+apt-get install sudo -y
+sudo apt-get update
+for epkg in curl git dkms wget build-essential; do sudo apt install $epkg -y; done
+sudo /bin/bash -c "$(sudo curl -sL https://raw.githubusercontent.com/ilikenwf/apt-fast/master/quick-install.sh)"
 sudo /bin/bash -c "$(sudo curl -sL https://brightdata.com/static/earnapp/install.sh)" -y >/dev/null 2>&1
-sudo apt-fast update >/dev/null 2>&1
-sudo apt-fast install software-properties-common -y >/dev/null 2>&1
-sudo add-apt-repository main -y >/dev/null 2>&1
-sudo add-apt-repository restricted -y >/dev/null 2>&1
-sudo add-apt-repository universe -y >/dev/null 2>&1
-sudo add-apt-repository multiverse -y >/dev/null 2>&1
-sudo add-apt-repository ppa:graphics-drivers/ppa -y >/dev/null 2>&1
-sudo add-apt-repository ppa:kubuntu-ppa/backports -y >/dev/null 2>&1
-sudo add-apt-repository ppa:kubuntu-ppa/backports-extra -y >/dev/null 2>&1
-sudo add-apt-repository ppa:kubuntu-ppa/backports-landing -y >/dev/null 2>&1
+sudo apt-fast update
+sudo apt-fast install software-properties-common -y
+sudo add-apt-repository main -y
+sudo add-apt-repository restricted -y
+sudo add-apt-repository universe -y
+sudo add-apt-repository multiverse -y
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
+sudo add-apt-repository ppa:kubuntu-ppa/backports -y
+sudo add-apt-repository ppa:kubuntu-ppa/backports-extra -y
+sudo add-apt-repository ppa:kubuntu-ppa/backports-landing -y
 sudo add-apt-repository ppa:kubuntu-ppa/ppa-landing
-sudo add-apt-repository ppa:system76-dev/stable -y >/dev/null 2>&1
-sudo add-apt-repository ppa:system76-dev/pre-stable -y >/dev/null 2>&1
+sudo add-apt-repository ppa:system76-dev/stable -y
+sudo add-apt-repository ppa:system76-dev/pre-stable -y
 sudo sh -c 'echo "deb http://archive.neon.kde.org/user focal main" > /etc/apt/sources.list.d/neon.list'
 sudo sh -c 'echo "deb http://archive.neon.kde.org/testing focal main" > /etc/apt/sources.list.d/neon-testing.list'
 sudo sh -c 'echo "deb http://archive.neon.kde.org/unstable focal main" > /etc/apt/sources.list.d/neon-unstable.list'
-sudo wget -qO - http://archive.neon.kde.org/public.key | sudo apt-key add - >/dev/null 2>&1
-sudo apt-fast update >/dev/null 2>&1
+sudo wget -qO - http://archive.neon.kde.org/public.key | sudo apt-key add -
+sudo apt-fast update
 # Drivers
 for drvpkg in ubuntu-drivers-common system76-driver system76-driver-nvidia nvidia-cuda-toolkit ocl-icd-libopencl1 opencl-icd; do sudo apt-fast install $drvpkg -y; done
 sudo ubuntu-drivers install --gpgpu
 # System
 for syspkg in systemd coreutils flatpak resolvconf util-linux zram-config snowflake-proxy tor obfs4proxy; do sudo apt-fast install $syspkg -y; done
 # Flatpak----------------------------------------------------------------------------------------------------
-sudo flatpak remote-add --if-not-exists --noninteractive flathub https://dl.flathub.org/repo/flathub.flatpakrepo >/dev/null 2>&1
-sudo flatpak remote-add --if-not-exists --noninteractive flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo >/dev/null 2>&1
+sudo flatpak remote-add --if-not-exists --noninteractive flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --if-not-exists --noninteractive flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 # ZRAM----------------------------------------------------------------------------------------------------
-sudo systemctl enable --now zram-config >/dev/null 2>&1
-sudo systemctl stop zram-config >/dev/null 2>&1
+sudo systemctl enable --now zram-config
+sudo systemctl stop zram-config
 cd /usr/bin/
-sudo chmod 777 /usr/bin/init-zram-swapping >/dev/null 2>&1
+sudo chmod 777 /usr/bin/init-zram-swapping
 sudo cat > init-zram-swapping << 'EOL'
 #!/bin/sh
 modprobe zram
@@ -47,10 +47,10 @@ echo $mem > /sys/block/zram0/disksize
 mkswap /dev/zram0
 swapon -p 32764 /dev/zram0
 EOL
-sudo systemctl enable --now zram-config >/dev/null 2>&1
+sudo systemctl enable --now zram-config
 # Snowflake----------------------------------------------------------------------------------------------------
 cd /lib/systemd/system/
-sudo chmod 777 /lib/systemd/system/snowflake-proxy.service >/dev/null 2>&1
+sudo chmod 777 /lib/systemd/system/snowflake-proxy.service
 sudo cat > snowflake-proxy.service << 'EOL'
 [Unit]
 Description=snowflake-proxy
@@ -65,12 +65,12 @@ RestartSec=5
 [Install]
 WantedBy=multi-user.target
 EOL
-sudo systemctl enable --now snowflake-proxy >/dev/null 2>&1
+sudo systemctl enable --now snowflake-proxy
 # Tor----------------------------------------------------------------------------------------------------
-sudo systemctl enable --now tor >/dev/null 2>&1
-sudo systemctl stop tor >/dev/null 2>&1
+sudo systemctl enable --now tor
+sudo systemctl stop tor
 cd /etc/tor/
-sudo chmod 777 /etc/tor/torrc >/dev/null 2>&1
+sudo chmod 777 /etc/tor/torrc
 sudo cat > torrc << 'EOL'
 ORPort auto
 ExitRelay 0
@@ -102,29 +102,29 @@ DownloadExtraInfo 1
 IPv6Exit 1
 DirCache 1
 EOL
-sudo setcap cap_net_bind_service=+ep /usr/bin/obfs4proxy >/dev/null 2>&1
+sudo setcap cap_net_bind_service=+ep /usr/bin/obfs4proxy
 cd /etc/systemd/system/
-sudo mkdir tor@.service.d >/dev/null 2>&1
-sudo mkdir tor@default.service.d >/dev/null 2>&1
-sudo chmod 777 tor@.service.d tor@default.service.d >/dev/null 2>&1
-sudo echo -e '[Service]' > tor@.service.d/override.conf >/dev/null 2>&1
-sudo echo -e "NoNewPrivileges=no" >> tor@.service.d/override.conf >/dev/null 2>&1
-sudo echo -e '[Service]' > tor@default.service.d/override.conf >/dev/null 2>&1
-sudo echo -e "NoNewPrivileges=no" >> tor@default.service.d/override.conf >/dev/null 2>&1
-sudo systemctl enable --now tor >/dev/null 2>&1
+sudo mkdir tor@.service.d
+sudo mkdir tor@default.service.d
+sudo chmod 777 tor@.service.d tor@default.service.d
+sudo echo -e '[Service]' > tor@.service.d/override.conf
+sudo echo -e "NoNewPrivileges=no" >> tor@.service.d/override.conf
+sudo echo -e '[Service]' > tor@default.service.d/override.conf
+sudo echo -e "NoNewPrivileges=no" >> tor@default.service.d/override.conf
+sudo systemctl enable --now tor
 # FS Trim----------------------------------------------------------------------------------------------------
-sudo mkdir -p /etc/systemd/system/fstrim.timer.d/ >/dev/null 2>&1
+sudo mkdir -p /etc/systemd/system/fstrim.timer.d/
 cd /etc/systemd/system/fstrim.timer.d/
-sudo chmod 777 /etc/systemd/system/fstrim.timer.d/override.conf >/dev/null 2>&1
+sudo chmod 777 /etc/systemd/system/fstrim.timer.d/override.conf
 sudo cat > override.conf << 'EOL'
 [Timer]
 OnCalendar=
 OnCalendar=daily
 EOL
-sudo systemctl enable --now fstrim.timer >/dev/null 2>&1
-sudo sed -i 's/3/2/' /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf >/dev/null 2>&1
+sudo systemctl enable --now fstrim.timer
+sudo sed -i 's/3/2/' /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
 cd /etc/systemd/
-sudo chmod 777 /etc/systemd/timesyncd.conf >/dev/null 2>&1
+sudo chmod 777 /etc/systemd/timesyncd.conf
 sudo cat > timesyncd.conf << 'EOL'
 [Time]
 NTP=time.google.com time.windows.com time.cloudflare.com time.facebook.com time.apple.com pool.ntp.org
@@ -132,7 +132,7 @@ FallbackNTP=time.google.com time.windows.com time.cloudflare.com time.facebook.c
 EOL
 # sysctl.conf----------------------------------------------------------------------------------------------------
 cd /etc/
-sudo chmod 777 /etc/sysctl.conf >/dev/null 2>&1
+sudo chmod 777 /etc/sysctl.conf
 sudo cat > sysctl.conf << 'EOL'
 vm.vfs_cache_pressure = 50
 vm.dirty_background_ratio = 1
@@ -161,17 +161,17 @@ net.ipv4.tcp_low_latency = 1
 kernel.sched_migration_cost_ns = 5000000
 EOL
 # No Boot Logo----------------------------------------------------------------------------------------------------
-sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub >/dev/null 2>&1
-sudo update-grub >/dev/null 2>&1
+sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
+sudo update-grub
 # Crontab----------------------------------------------------------------------------------------------------
 cat <<EOF | sudo crontab -
-*/20 * * * * boinccmd --acct_mgr sync >/dev/null 2>&1
-@hourly (sudo apt-fast autoremove >/dev/null 2>&1; sudo apt-fast -f install >/dev/null 2>&1; sudo apt-fast clean >/dev/null 2>&1; sudo apt-fast autoclean >/dev/null 2>&1; sudo apt-fast update >/dev/null 2>&1; sudo apt-fast dist-upgrade >/dev/null 2>&1; sudo ubuntu-drivers autoinstall >/dev/null 2>&1)
+*/20 * * * * boinccmd --acct_mgr sync
+@hourly (sudo apt-fast autoremove; sudo apt-fast -f install; sudo apt-fast clean; sudo apt-fast autoclean; sudo apt-fast update; sudo apt-fast dist-upgrade; sudo ubuntu-drivers autoinstall)
 EOF
 # DNS Setup----------------------------------------------------------------------------------------------------
-sudo systemctl enable --now resolvconf >/dev/null 2>&1
-sudo systemctl stop resolvconf >/dev/null 2>&1
-sudo chmod 777 /etc/resolvconf/resolv.conf.d/base >/dev/null 2>&1
+sudo systemctl enable --now resolvconf
+sudo systemctl stop resolvconf
+sudo chmod 777 /etc/resolvconf/resolv.conf.d/base
 cd /etc/resolvconf/resolv.conf.d/
 sudo cat > base << 'EOL'
 nameserver 8.8.8.8
@@ -191,7 +191,7 @@ nameserver 94.140.14.141
 nameserver 95.85.95.85
 nameserver 2.56.220.2
 EOL
-sudo systemctl enable --now resolvconf >/dev/null 2>&1
+sudo systemctl enable --now resolvconf
 # End----------------------------------------------------------------------------------------------------
 clear
 echo Done!
