fb0cd07fe6424ff793cec9863c3c010ca9e1d215
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-07-09 10:23:25 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -22,7 +22,7 @@ CombinedUpgrade
 EOL'
 # Powerpill----------------------------------------------------------------------------------------------------
 # Setup
-
+paru -Syu powerpill
 # Chaotic-AUR----------------------------------------------------------------------------------------------------
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
