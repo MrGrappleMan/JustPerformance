f51094d97c5040bbb93d638fdd84949755268eef
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-07-09 09:59:05 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -18,7 +18,8 @@ SkipReview
 SudoLoop
 CompletionInterval = 1
 SaveChanges
-CombinedUpgrade'
+CombinedUpgrade
+EOL'
 # Chaotic-AUR----------------------------------------------------------------------------------------------------
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
