ef6f332f8510eb9565cfde58d2197bed251647a5
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-07-09 07:45:13 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -8,6 +8,7 @@ cd /tmp/paru-git
 makepkg -si --noconfirm
 cd
 # Config
+
 # Chaotic-AUR----------------------------------------------------------------------------------------------------
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
