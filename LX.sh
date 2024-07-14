b89914903cc3ecdd30d9c73358b8394f28f8f712
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-07-14 16:34:05 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -9,7 +9,7 @@ git clone https://aur.archlinux.org/paru-git.git /tmp/paru-git
 cd /tmp/paru-git
 makepkg -si --noconfirm
 # Config
-cat > /etc/paru/paru.conf << "ASDF"
+cat > /etc/paru/paru.conf << "XIT"
 [options]
 PgpFetch
 Devel
@@ -20,10 +20,10 @@ SudoLoop
 CompletionInterval = 1
 SaveChanges
 CombinedUpgrade
-nd
+XIT
 # Powerpill----------------------------------------------------------------------------------------------------
 # Setup
-paru -Syu powerpill
+paru -Syu -powerpill
 # Chaotic-AUR----------------------------------------------------------------------------------------------------
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
