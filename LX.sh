b9cc43c0756783361bb3cc86a322e9e091ab5d21
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-08-15 12:55:07 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -1088,6 +1088,7 @@ for pakges in\
  pipewire-git libpipewire-git wireplumber-git libwireplumber-git\
  hyprland-git eww-git\
  flatpak paru-git mc pi-hole-standalone snowflake-pt-proxy
+do paru -Syu --noconfirm $
 if [[ "$1" == "N" ]]; then
  paru -Syu --noconfirm nvidia-open-dkms-git opencl-nvidia-beta nvidia-utils-beta nvidia-settings-beta nvidia-vpf-git nvflash
 fi
