ef49f63a6df8686c59749aba7180f130e5751b1e
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-08-15 11:02:03 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -44,6 +44,7 @@ CompletionInterval = 1
 SudoLoop
 SkipReview
 XIT
+cd
 rm -rf paru-git
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
