05bc66461713b421dbda7da75ea5fc7c5c9a32d2
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-08-24 02:42:49 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -41,12 +41,12 @@ Include = /etc/pacman.d/mirrorlist
 Include = /etc/pacman.d/mirrorlist
 [multilib]
 Include = /etc/pacman.d/mirrorlist
-[chaotic-aur]
-Include = /etc/pacman.d/chaotic-mirrorlist
 [kde-unstable]
 Include = /etc/pacman.d/mirrorlist
 [gnome-unstable]
 Include = /etc/pacman.d/mirrorlist
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
 XIT
 sudo rm -rf /var/lib/pacman/db.lck /var/cache/pacman/pkg/* ~/paru-git
 sudo pacman -Syy --noconfirm base-devel git
