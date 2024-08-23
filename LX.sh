49771ee3477e8432e23d0aef29bbc6d443c618ee
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-08-23 19:55:06 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -16,7 +16,7 @@ if SWAPDEV=$(grep -e "^/dev/zram" /proc/swaps | awk '{print $1}'); then
 fi
 sudo rmmod zram
 sudo modprobe zram
-CORENO=$(nproc)
+sudo CORENO=$(nproc)
 sudo echo $CORENO > /sys/class/zram-control/hot_add
 sudo mem=$(((LC_ALL=C free | grep -e "^Mem:" | sed -e "s/^Mem: *//" -e "s/  *.*//") * 1024))
 sudo echo $mem > /sys/block/zram0/disksize
