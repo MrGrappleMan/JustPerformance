80e896828056a7a304d9a680dbb2a5fedc2b7666
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-08-23 22:47:41 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -148,8 +148,6 @@ if [[ "$1" == "Y" ]]; then
 modprobe zram
 totalmem=((`LC_ALL=C free | grep -e "^Mem:" | sed -e 's/^Mem: *//' -e 's/  *.*//'`)*1024)
 mem=$((totalmem / 2 * 1024))
-
-# initialize the devices
 echo $mem > /sys/block/zram0/disksize
 mkswap /dev/zram0
 swapon -p 5 /dev/zram0
