d94db462fb5e307c0f46b03a0a200817d997cf5d
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-08-25 01:25:53 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -20,12 +20,15 @@ sudo swapon -p 32765 ~/swapfile
 sudo sysctl vm.swappiness=1
 # PackageMgmt---------------------------------------------------------------------------------------------------- 
 sudo rm -rf /var/lib/pacman/db.lck /etc/pacman.d/gnupg /var/lib/pacman/sync/* ~/paru-git
+sudo pacman -Sc --noconfirm
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
-sudo pacman -Sc
 sudo touch /etc/pacman.conf
 sudo chmod 777 /etc/pacman.conf
-
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 sudo cat > /etc/pacman.conf << "XIT"
 [options]
 HoldPkg = pacman glibc paru-git
