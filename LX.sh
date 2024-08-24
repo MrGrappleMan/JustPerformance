d799a9a3d1b526ef3e7a2e836674a39dab305ce9
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-08-24 17:24:43 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -58,8 +58,8 @@ cd paru-git
 sudo renice -n -20 -p $$
 makepkg -si --noconfirm
 sudo renice -n 0 -p $$
+sudo rm -rf ~/paru-git
 cd
-sudo rm -rf paru-git
 sudo touch /etc/paru.conf
 sudo chmod 777 /etc/paru.conf
 sudo cat > /etc/paru.conf << "XIT"
