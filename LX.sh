c02be5f4d00e99403ce88cfcde70cfd67c4c0561
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-08-26 23:59:32 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -106,6 +106,7 @@ sudo touch /usr/bin/JPpkg
 sudo chmod 777 /usr/bin/JPpkg
 sudo cat > /usr/bin/JPpkg << "XIT"
 #!/bin/bash
+sudo systemctl enable 
 if [[ "$1" == "upd" ]]; then
 sudo reflector --latest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 paru -Syyu --noconfirm
