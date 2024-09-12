9ce92ed6fefa1f602b3ffeacd6104e7c2781ed2d
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-09-12 19:21:33 +0530

    update LXprepare.sh

--- LXprepare.sh
+++ LXprepare.sh
@@ -1,1 +1,24 @@
 #!/bin/zsh
+netrf() {
+    while true; do
+        sudo -v
+        sleep 240
+    done
+}
+netrf &
+NETRP=$!
+sudo swapoff -a
+sudorefresh() {
+    while true; do
+        sudo -v
+        sleep 240
+    done
+}
+sudorefresh &
+SUDOREFRESHP=$!
+sudo touch ~/swapfile
+sudo fallocate -l 8G ~/swapfile
+sudo chmod 755 ~/swapfile
+sudo mkswap ~/swapfile
+sudo swapon -p 32765 ~/swapfile
+sudo sysctl vm.swappiness=1
