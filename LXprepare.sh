d989db2ee1fc2ef14d9fb0cd2a505db29606af69
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-09-12 18:20:03 +0530

    update LXprepare.sh

--- LXprepare.sh
+++
@@ -1,8 +0,0 @@
-# Bruh, dont forgor 💀 to mount yo disk 
-pacstrap /mnt base linux linux-firmware
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-pacman -Syu base base-devel git
-exit
-umount -R /mnt
-reboot
