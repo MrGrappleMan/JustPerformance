90feb7851e6c0fc7974db5daa432e4353261b75a
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-09-12 20:15:43 +0530

    update LXprepare.sh

--- LXprepare.sh
+++ LXprepare.sh
@@ -23,7 +23,7 @@ sudo chmod 755 ~/swapfile
 sudo mkswap ~/swapfile
 sudo swapon -p 32765 ~/swapfile
 sudo sysctl vm.swappiness=1
-sudo pacman -Sy vase bas
+sudo pacman -Sy base base-devel git
 kill $SUDOREFRESHP
 kill $NETRFP
 exit
