363fe7b89d0e88e257c1df87681e1cd13ae03f8f
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2024-10-02 12:16:11 +0530

    update LX.sh

--- LX.sh
+++ LX.sh
@@ -167,16 +167,26 @@ if [[ "$1" == "Y" ]]; then
     sudo echo $mem_total > /sys/block/zram0/disksize
     sudo mkswap /dev/zram0
     sudo swapon -p 32765 /dev/zram0
+
     set_compression_level() {
         local level=$1
         echo "zstd:$level" > /sys/block/zram0/comp_algorithm
     }
+
+    set_swappiness() {
+        local swappiness=$1
+        sysctl -w vm.swappiness=$swappiness
+    }
+
     monitor_memory() {
         local part_size=$(($mem_total / 22))
         while true; do
             mem_free=$(awk '/MemAvailable/ {print $2}' /proc/meminfo)
             level=$((22 - mem_free / part_size))
             set_compression_level $((level < 1 ? 1 : level > 22 ? 22 : level))
+            swappiness=$((200 - (mem_free * 200 / mem_total)))
+            set_swappiness $((swappiness < 0 ? 0 : swappiness > 200 ? 200 : swappiness))
+
             sleep 60
         done
     }
