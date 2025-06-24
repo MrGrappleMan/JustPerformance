0f2d857442c673a8228b7f8048170a7a267be3d6
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-06-24 12:24:09 +0530

    Update jp.bat

--- jp.bat
+++ jp.bat
@@ -6,7 +6,7 @@ set el=!errorlevel!
 if !el!==2 (
 color 04
 cls
-echo Please run with administrator rights!
+echo Re-run with admin rights
 echo Press any key to exit...
 pause>nul
 exit
@@ -15,9 +15,12 @@ set spr=echo ___________________________________________________________________
 set svcopt="if !el!==1 (sc stop "!svcnme!" ^& sc config "!svcnme!" start=disabled) ^& if !el!==2 (sc start "!svcnme!" ^& sc config "!svcnme!" start=auto)"
 set dsoren="%spr% ^& echo Options: ^& echo X.Return ^& echo 1.Disable ^& echo 2.Enable ^& choice /C 12X /N"
 color 07
-powercfg -h off
+powercfg -h on
 ::If you doubt the line below this, reddit.com/r/computers/s/Pa11pjBory
 sc start "SysMain">nul & sc config "SysMain" start=auto>nul
+sc stop "WSearch">nul & sc config "WSearch" start=disabled>nul
+powercfg.exe -import "!cd!\JPNormal.pow">nul
+w32tm /config /syncfromflags:manual /manualpeerlist:"time.google.com time.windows.com time.cloudflare.com time.facebook.com time.apple.com" /reliable:YES /update & net stop w32time & net start w32time & w32tm /resync /force
 regedit /s jp.reg
 :home
 cls
@@ -38,19 +41,15 @@ echo Consider rebooting after exiting for all changes to take effect.
 echo Options:
 echo X.Exit
 echo 1.Recommended Browser Flags
-echo 2.Search Indexing (WSearch)
-echo 3.Printing (Spooler, Fax)
-echo 4.Windows Image Acquisition (StiSvc)
-echo 5.Blutooth (BTAGService, bthserv)
-echo 6.Remote Desktop (SessionEnv, TermService, UmRdpService)
-echo 7.Remote Registry (RemoteRegistry)
-echo 8.Time Tweaks
-echo 9.System accuracy(HPET and Dynamic Ticks)
-echo A.Hypervisor
-echo B.Pagedisk Creator
-echo C.Audio
-echo D.Import Dedicated Power Plans
-choice /C 123456789ABCDX /N
+echo 2.Printing (Spooler, Fax)
+echo 3.Windows Image Acquisition (StiSvc)
+echo 4.Blutooth (BTAGService, bthserv)
+echo 5.Remote Desktop (SessionEnv, TermService, UmRdpService, RemoteRegistry)
+echo 6.System accuracy(HPET and Dynamic Ticks)
+echo 7.Hypervisor
+echo 8.Pagedisk Creator
+echo 9.Audio Engine
+choice /C 123456789X /N
 cls
 if !el!==1 (
 type chromium.txt
@@ -60,22 +59,10 @@ pause>nul
 goto home
 )
 if !el!==2 (
-	echo Name: Windows Search(WSearch)
-	echo Type: Service
-	echo Indexes and searches files. Improves the speed of searches and provides more accurate results.
-	echo By disabling it, searches would take some more time including explorer.exe, but would give a performance boost. If you use system utilites often,
-	echo you can make a habit of opening them using Run.exe for fast access if you know their filenames. Desktop icons are also recommended.
-	echo Use 'shell:AppsFolder' in run.exe to create shortcuts of UWP apps..
-	!dsoren!
-	set svcnme=WSearch
-	!svcopt!
-	goto home
-)
-if !el!==3 (
 	echo Name: Printing(Spooler, Fax)
+        echo Disable if you do not use a printer. This considers virtual printers as well.
 	echo Manages print jobs sent from the computer to the printer or print server.
 	echo It can store multiple print jobs in the print queue or buffer retrieved by the printer or print server.
-	echo Runs in the background regardless whether you have a printer or not.
 	!dsoren!
 	set svcnme=Spooler
 	!svcopt!
@@ -83,17 +70,18 @@ if !el!==3 (
 	!svcopt!
 	goto home
 )
-if !el!==4 (
+if !el!==3 (
 	echo Name: Windows Image Acquisition(StiSvc)
 	echo Waits until you press the button on your scanner and then manages the process of getting the image where it needs to go.
-	echo This also affects communication with digital cameras and video cameras that you connect directly to your computer, so be aware of that if you need this function.
+	echo This also affects communication with cameras and Android PTP that you connect directly to your computer, so be aware of that if you need this function.
 	!dsoren!
 	set svcnme=StiSvc
 	!svcopt!
 	goto home
 )
-if !el!==5 (
+if !el!==4 (
 	echo Name: Blutooth(BTAGService, bthserv)
+        echo Disable if you do not use bluetooth
 	echo BTAGService: Service supporting the audio gateway role of the Bluetooth Handsfree Profile.
 	echo bthserv: The Bluetooth service supports discovery and association of remote Bluetooth devices.
 	echo Stopping or disabling this service may cause paired Bluetooth devices to fail to operate properly and prevent new devices from being discovered or paired.
@@ -105,14 +93,14 @@ if !el!==5 (
 	!svcopt!
 	goto home
 )
-if !el!==6 (
-	echo Name: Remote Desktop(SessionEnv, TermService, UmRdpService)
+if !el!==5 (
+	echo Name: Remote Access(SessionEnv, TermService, UmRdpService, RemoteRegistry)
 	echo These services make remote control of your computer possible.
-	echo If you don't use the remote desktop functionality of Windows, disable all three of these services.
-	echo However, Microsoft Support can use remote desktop technology to help fix issues you might be experiencing.
+	echo If you don't use the remote desktop functionality of Windows, disable all these services.
+	echo However, Microsoft Support can potentially use remote desktop technology to diagnose issues you might be experiencing.
 	echo Windows's Remote support won't work if you disable these services.
 	echo This may also be a serious security issue and is used in fake support scams.
-	echo So, disabling these services can also help improve the security and performance of your computer.
+	echo So, disabling these services can also help improve the security and provide some performance to your computer.
 	echo You may use Chrome Remote Desktop or most other 3rd party apps without any issues.
 	!dsoren!
 	set svcnme=SessionEnv
@@ -121,56 +109,32 @@ if !el!==6 (
 	!svcopt!
 	set svcnme=UmRdpService
 	!svcopt!
-	goto home
-)
-if !el!==7 (
-	echo Name: Remote Registry(RemoteRegistry)
-	echo Enables remote users to modify registry settings on this computer.
-	echo If this service is stopped, the registry can be modified only by users on this computer.
-	echo If this service is disabled, any services that explicitly depend on it will fail to start.
-	echo Use only if you want to make registry changes remotely to your computer.
-	echo Leaving it enabled may compromise your security.
-	!dsoren!
-	set svcnme=StiSvc
+        set svcnme=RemoteRegistry
 	!svcopt!
 	goto home
 )
-if !el!==8 (
-	echo Name: NTP Tweaks
-	echo Modifies w32tm to refresh system time every hour.
-	echo Uses many reliable NTP servers along with backup.
-	echo Use if not using a device provided by an organisation.
-	%spr%
-	echo Options:
-	echo X.Return
-	echo 1.Apply (Preferred for most users)
-	choice /C 1X /N
-	if !el!==1 (w32tm /config /syncfromflags:manual /manualpeerlist:"time.google.com time1.google.com time2.google.com time3.google.com time4.google.com time.windows.com time.cloudflare.com 0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org time.facebook.com time1.facebook.com time2.facebook.com time3.facebook.com time4.facebook.com time5.facebook.com time.apple.com time1.apple.com time2.apple.com time3.apple.com time4.apple.com time5.apple.com time6.apple.com time7.apple.com" /reliable:YES /update & net stop w32time & net start w32time & w32tm /resync /force)
-	goto home
-)
-if !el!==9 (
+if !el!==6 (
 	echo Name: System accuracy(HPET and Dynamic Ticks)
 	echo Type: Program
 	echo This command forces the kernel timer to constantly poll for interrupts instead of wait for them.
 	echo Dynamic tick was implemented as a power saving feature for laptops but hurts desktop performance.
-	echo Do not change on laptops or as a result, battery life will affected.
+	echo You should not change this
 	!dsoren!
 	if !el!==1 (bcdedit /set disabledynamictick no & bcdedit /set useplatformclock true)
 	if !el!==2 (bcdedit /set disabledynamictick yes & bcdedit /deletevalue useplatformclock)
 	goto home
 )
-if !el!==10 (
+if !el!==7 (
 	echo Name: Hypervisor(using bcdedit)
 	echo Type: Program
 	echo Used for VMs, Emulators and Subsystems.
-	echo Useless and consumes extra resources if not utilized by any program.
 	echo Disable if not in use.
 	!dsoren!
 	if !el!==1 (bcdedit /set hypervisorlaunchtype off)
 	if !el!==2 (bcdedit /set hypervisorlaunchtype on)
 	goto home
 )
-if !el!==11 (
+if !el!==8 (
 	echo Name: Pagedisk
 	echo This can make an NTFS partition with 2MB allocation unit size. Use the partition to keep a pagefile in it,
 	echo giving your device a faster read and write speed to the file compared to storing it on a regular 4KB partition.
@@ -200,36 +164,14 @@ if !el!==11 (
 		goto home
 	)
 )
-if !el!==12 (
+if !el!==9 (
 	echo Name: Audio (Audiosrv,AudioEndpointBuilder)
-	echo Controls and enables functionality of audio on Windows.
+	echo Windows audio engine. Expect no performance boost unless your device is a potato. Not recommended to disable.
 	!dsoren!
 	set svcnme=Audiosrv
 	!svcopt!
 	set svcnme=AudioEndpointBuilder
 	!svcopt!
-	goto home
-)
-if !el!==13 (
-	echo Name: Dedicated Power Plans
-	echo Overheating is the cause of how your system handles heavy load, which is modified by this.
-	echo Use your UEFI/BIOS setup utility if it has any options or SpeedFan for boosting fan speed. Laptop manufacturers may keep it locked.
-	echo It is recommended to switch the Power Plan from JPIntensive to JPNormal once your resource intensive task(s) has been completed.
-	echo Imports 3 power plans. Use each power plan according to your needs only. Use the Intensive variant at your own risk.
-	echo The automatic variant is the best variant for portable devices with a battery.
-	echo JPNormal: Maximizes all options except for processor to use resources in a performant manner.
-	echo JPIntensive: Device is forced to achieve the best possible performance.
-	echo JPAutomatic: Power management and Performance are automatically determined comparatively better than Windows' Balanced power plan. 
-	%spr%
-	echo Options:
-	echo X.Return
-	echo 1.Import
-	choice /C 1X /N
-	if !el!==1 (
-		powercfg.exe -import "!cd!\JPNormal.pow">nul
-		powercfg.exe -import "!cd!\JPIntensive.pow">nul
-		powercfg.exe -import "!cd!\JPAuto.pow">nul
-	)
 	goto home
 )
 net stop wuauserv>nul
@@ -242,6 +184,7 @@ ipconfig /registerdns>nul
 ipconfig /release>nul
 ipconfig /renew>nul
 wuauclt.exe /updatenow
-net start wuauserv>nul
+net start wuauserv>nul
+
 exit
 endlocal
